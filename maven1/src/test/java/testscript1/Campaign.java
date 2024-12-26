4c156327754e6709c4df83507ade32af87ec0ac2
Dell <Dell@KOUSHIK>
2024-12-26 09:31:40 +0530

    commit

---
+++ maven1/src/test/java/testscript1/Campaign.java
@@ -0,0 +1,14 @@
+package testscript1;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Campaign {
+
+	@Test
+	
+	public void createCampaign() {
+		Reporter.log("createCampaign",true);
+	}
+
+
+}
